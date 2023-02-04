1140c2fb0f151512845615b043243a1a96eb39bd
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-04 15:18:36 +0900

    userテーブルの作成

--- db/migrate/20230204060840_devise_create_users.rb
+++ db/migrate/20230204060840_devise_create_users.rb
@@ -4,6 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
+      t.string :surname,            null: false
+      t.string :firstname,          null: false
+      t.string :surname_kana,       null: false
+      t.string :firstname_kana,     null: false
+      t.date :birthday,             null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
