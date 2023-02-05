f4a905e6bbac22dd58f569aff3e279dd8a03a85e
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-05 14:01:19 +0900

    ログインの有無で表示内容が変わる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :surname, :firstname, :surname_kana, :firstname_kana, :birthday])
+  end
 end
