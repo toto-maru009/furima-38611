f5440923fa042d1035bf98556fc2255c791940b0
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-07 15:58:06 +0900

    ログインしてないと出品できない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+  end
+
+  private
+
 end
