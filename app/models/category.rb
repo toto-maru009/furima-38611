d86f4799a363799e690de19c7856aece74af450b
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-08 12:52:25 +0900

    categoryの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,10 @@
 class Category < ActiveHash::Base
-  self.date = [
+  self.data = [
     { id: 1, name: '---' },
     { id: 2, name: 'メンズ' },
     { id: 3, name: 'レディース' },
     { id: 4, name: 'ベビー' },
-    { id: 5, name: 'キッズ'}
+    { id: 5, name: 'キッズ'},
     { id: 6, name: 'インテリア・住まい・小物' },
     { id: 7, name: '本・音楽・ゲーム' },
     { id: 8, name: 'おもちゃ・ホビー・グッズ' },
@@ -14,7 +14,7 @@ class Category < ActiveHash::Base
     { id: 12, name: 'その他' }
   ]
 
-  Include ActiveHash::Associations
+  include ActiveHash::Associations
     has_many :items
 
 end
