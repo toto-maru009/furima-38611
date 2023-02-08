183551b64bff3ab06f95e4351874235729238792
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-08 13:24:46 +0900

    ActiveHash periodの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,16 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :status
+  belongs_to :payee
+  belongs_to :prefecture
+  belongs_to :period
 
   validates :name, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :payee_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :period_id, numericality: { other_than: 1 , message: "can't be blank" }
 
 
 end
