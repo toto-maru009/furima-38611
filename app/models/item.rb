1988fd1b009f671069a1b2bd116bb40ebf7f6e41
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-08 12:59:54 +0900

    ActiveHash statusの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :category
+  belongs_to :status
 
   validates :name, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
+
 
 end
