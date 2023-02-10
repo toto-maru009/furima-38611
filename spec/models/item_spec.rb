0977aa5237cf024157244466517eaf9a8c516ac2
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-10 22:08:25 +0900

    単体テストコードの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,6 +27,51 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
+      it 'カテゴリーが初期値だと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態が初期値だと出品できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '配送料の負担が初期値だと出品できない' do
+        @item.payee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Payee can't be blank")
+      end
+      it '発送元の地域が初期値だと出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数が初期値だと出品できない' do
+        @item.period_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Period can't be blank")
+      end
+      it '販売価格が空だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '販売価格が半角数字以外だと出品できない' do
+        @item.price ='KKK'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 半角数字で¥300~¥9,999,999の間のみ出品可能")
+      end
+      it '販売価格が300以下だと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 半角数字で¥300~¥9,999,999の間のみ出品可能")
+      end
+      it '販売価格が99999999以上だと出品できない' do
+        @item.price = 100000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 半角数字で¥300~¥9,999,999の間のみ出品可能")
+      end
     end
   end
 end
