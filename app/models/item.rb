e76a17beae0682066909431ba98d70797a6c1e4a
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-10 18:21:20 +0900

    テストの実装(途中), バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :explanation, presence: true
+  validates :image, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 99999999 , message: "¥300~¥9,999,999の間のみ保存可能"}
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品管理機能' do
+    context '出品できる場合' do
+      it 'すべての値が規定通りに存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '出品できない場合' do
+      it '商品名が空だと出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '説明が空だと出品できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it '画像が空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+    end
+  end
 end
