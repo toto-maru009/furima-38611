7205aca2ee4fc3c24aa94d64d7c1f666510a466d
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-11 21:27:34 +0900

    コントローラーの編集　ログインしてないとログイン画面に移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,10 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:new]
   def index
   end
 
   def new
     @item = Item.new
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
 
   def create
