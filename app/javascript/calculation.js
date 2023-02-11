d2c1bdb37695f76f71ca6de0d8dd0fdbe5ef0d0c
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-02-11 19:52:55 +0900

    javascriptの変更　少数を切り捨てるように記述

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -2,8 +2,8 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = Math.round(priceInput.value * 0.1 );
+    addTaxPrice.innerHTML = Math.floor(priceInput.value * 0.1 );
     const addPofit = document.getElementById("profit");
-    addPofit.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+    addPofit.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
  })
 });
