cc6477e69809b4fd3607cdb674d7a810bfb8f465
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-03-06 17:42:22 +0900

    テーブルの設計

--- db/migrate/20230306082244_create_buyers.rb
+++ db/migrate/20230306082244_create_buyers.rb
@@ -1,7 +1,13 @@
 class CreateBuyers < ActiveRecord::Migration[6.0]
   def change
     create_table :buyers do |t|
-
+      t.string :postal_code,      null: false
+      t.string :municipality,     null: false
+      t.string :address,          null: false
+      t.string :building_name,    null: true
+      t.string :telephone_number, null: false
+      t.integer :prefecture_id,   null: false
+      t.references :purchase,     null: false, foreign_key: true
       t.timestamps
     end
   end
