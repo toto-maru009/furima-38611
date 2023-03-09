3e0ee8cf4b420cf74332c45e2fdbe5146aa95e37
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-03-09 15:09:44 +0900

    saveクラスの定義

--- app/models/purchase_buyer.rb
+++ app/models/purchase_buyer.rb
@@ -11,4 +11,11 @@ class PurchaseBuyer
     validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "10桁以上11桁以内の半角数値のみ"  }
   end
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+
+  def save
+    # 購入についての情報を変数purchaseに代入
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    #住所の保存
+    Buyer.create(postal_code: postal_code, :municipality municipality, :address address, :building_name building_name, :telephone_number telephone_number, :prefecture_id prefecture_id purchase_id: purchase_id)
+  end
 end
