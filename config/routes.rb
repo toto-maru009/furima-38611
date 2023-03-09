f19f6990f3ef123d3cbc216e50bd630ee54c5a65
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-03-09 17:00:34 +0900

    routesの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'items/index'
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :purchases, only: [:index, :create]
+
+  resources :items do
+    resources :purchases, only: [:index, :create]
+  end
 end
