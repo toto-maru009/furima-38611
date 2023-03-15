cbc8c322b488383df9ec7bd2227da79646013a1e
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-03-16 01:31:10 +0900

    purchase_buyerのfactorybotの編集

---
+++ spec/factories/purchase_buyer.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_buyer do
+    postal_code {}
+    municipality {}
+    address {}
+    telephone_number {}
+    token {}
+    building_name {}
+    prefecture_id {}
+  end
+end
