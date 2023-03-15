cb69a79606e852cd742eb474c6fff293e7d6cb7f
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-03-16 01:16:24 +0900

    モデルにtokenを追加

--- app/models/purchase_buyer.rb
+++ app/models/purchase_buyer.rb
@@ -1,6 +1,6 @@
 class PurchaseBuyer
   include ActiveModel::Model
-  attr_accessor :postal_code, :municipality, :address, :building_name, :telephone_number, :prefecture_id, :user_id, :item_id
+  attr_accessor :postal_code, :municipality, :address, :building_name, :telephone_number, :prefecture_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
