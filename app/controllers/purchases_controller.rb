61c9805d63b736cc98328a263c9ea033e4ae281e
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-03-17 11:14:17 +0900

    factorybotの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -20,7 +20,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_buyer).permit(:municipality, :address, :building_name, :telephone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token] )
+    params.require(:purchase_buyer).permit(:postal_code,:municipality, :address, :building_name, :telephone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token] )
   end
 
   def set_item
--- spec/factories/purchase_buyer.rb
+++ spec/factories/purchase_buyer.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :purchase_buyer do
-    postal_code {}
-    municipality {}
-    address {}
-    telephone_number {}
-    token {}
-    building_name {}
-    prefecture_id {}
+    postal_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    municipality { Faker::Address.city }
+    address { Faker::Address.street_address }
+    telephone_number { Faker::Number.decimal_part(digits: 11) }
+    token { Faker::Internet.password(min_length: 20, max_length: 30) }
+    building_name { Faker::Address.street_address }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
   end
 end
