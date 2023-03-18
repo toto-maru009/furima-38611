fd9e7b1b6d1af7a26db9e7cf31838cae4891ae58
toto-maru009 <cyborg.kohei002@outlook.jp>
2023-03-18 21:41:37 +0900

    テストコードの生成

---
+++ spec/models/purchase_buyer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseBuyer, type: :model do
+  describe '購入管理機能' do
+    context '商品が購入できるとき' do
+      it 'すべての値が規定通りに存在すれば登録できる' do
+      end
+      it '建物名が空でも登録できる' do
+      end
+    end
+  end
+end
